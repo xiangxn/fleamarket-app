299567bd3a2b7fc6f4a2fb389e6e75c58b2ab6c3
bulangnisi <231641390@qq.com>
2020-06-22 17:40:03 +0800

    delete import dart js

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
-import 'dart:js';
 void main(List<String> args) {
   WidgetsFlutterBinding.ensureInitialized();
   ExtLocale locale = ExtLocale();
