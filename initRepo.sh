0a602082346a3a58f06d8c055619a211562edccc
necklace <xiangxn@163.com>
2020-11-25 21:22:57 +0800

    update init repo code

--- initRepo.sh
+++ initRepo.sh
@@ -1,6 +1,7 @@
 mkdir tmp &&
 cd tmp &&
 git clone git@btsgo.net:fleamarket-app.git &&
+git checkout xndev && git reset --hard 2bcb1bb5966ea786cc1c7248c926ad822b137cd4 &&
 cd ..
 cp ./tmp/fleamarket-app/ios/Runner.xcodeproj/project.pbxproj ./ios/Runner.xcodeproj/project.pbxproj
 cp ./tmp/fleamarket-app/ios/Runner.xcworkspace/contents.xcworkspacedata ./ios/Runner.xcworkspace/contents.xcworkspacedata &&
